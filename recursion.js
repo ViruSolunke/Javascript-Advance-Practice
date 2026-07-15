9ef46b3abaaba8a0a938815c048f56928906a35d
ViruSolunke <virendrasolunke14@gmail.com>
2026-07-15 12:33:16 +0530

    Range of Numbers Generator  program

--- recursion.js
+++ recursion.js
@@ -24,3 +24,31 @@ console.log(countdown(0));
 
 console.log(countdown(-1));
 // []
+
+
+// Range Count
+
+function rangeOfNumbers(startNum, endNum) {
+  // Base case
+  if (startNum === endNum) {
+    return [startNum];
+  }
+
+  // Recursive case
+  return [startNum].concat(rangeOfNumbers(startNum + 1, endNum));
+}
+
+console.log(rangeOfNumbers(1, 5));
+// [1, 2, 3, 4, 5]
+
+console.log(rangeOfNumbers(6, 9));
+// [6, 7, 8, 9]
+
+console.log(rangeOfNumbers(4, 4));
+// [4]
+
+console.log(rangeOfNumbers(10, 15));
+// [10, 11, 12, 13, 14, 15]
+
+console.log(rangeOfNumbers(2, 8));
+// [2, 3, 4, 5, 6, 7, 8]
