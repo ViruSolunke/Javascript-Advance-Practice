47393414c890dbd3edd15b8de890a3d25c4793d7
ViruSolunke <virendrasolunke14@gmail.com>
2026-07-17 22:54:01 +0530

    Sort method Example added

--- Algotithms/Selection_Sort_Implimentation.js
+++ Algotithms/Selection_Sort_Implimentation.js
@@ -29,3 +29,79 @@ function selectionSort(arr) {
 console.log(
   selectionSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])
 );
+
+// ==========================================================
+// Another Example of Selection Sort =>
+
+// ===========================================================
+
+// Ques 2: Implement Selection Sort in JavaScript
+// Write a function to sort the given array nums in ascending order.
+
+// Input: nums = [29,10,14,37,14]  ----->>>>>  Output: [10,14,14,29,37]
+
+const selectionSort = (arr) => {
+  const n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    // n
+    let minIndex = i;
+    for (let j = i + 1; j < n; j++) {
+      // n
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+};
+
+console.log(selectionSort([29, 10, 14, 37, 14]));
+
+// Time Complexity = O(n^2)
+// Space Complexity = O(1)
+
+
+// ==================================================
+// Sort() Method =>
+
+// =================================================
+
+
+// Sort() Method in Javascript
+
+const fruits = ["dragon fruit", "apple", "date", "cherry", "banana"];
+
+console.log(fruits.sort());
+
+const nums = [10, 5, 18, 1, 27];
+
+const compareFunction = (a, b) => {
+  // 1. <0 => a comes first
+  // 2. 0  => nothing changes
+  // 3. >0 => b comes first
+
+  return b - a;
+};
+
+console.log(nums.sort(compareFunction));
+
+const people = [
+  {name: "Piyush", age: 25},
+  {name: "Suyash", age: 22},
+  {name: "Raj", age: 30},
+  {name: "Ishan", age: 27},
+];
+
+const compareFunctionForObj = (a, b) => {
+  // 1. <0 => a comes first
+  // 2. 0  => nothing changes
+  // 3. >0 => b comes first
+
+  return a.age - b.age;
+};
+
+console.log(people.sort(compareFunctionForObj));
