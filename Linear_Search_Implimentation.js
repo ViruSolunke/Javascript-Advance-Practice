a1009e4a98d50d999f5ee8cfea4cc32bba35d76f
ViruSolunke <virendrasolunke14@gmail.com>
2026-07-17 09:15:43 +0530

    Linear Search implimentation

---
+++ Linear_Search_Implimentation.js
@@ -0,0 +1,12 @@
+let arr = [13, 4, 7, 9, 10];
+
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+console.log(linearSearch(arr, 7));
